e06ed49448980861d1776ef1ae236373421a0570
Hazar NAMDAR <hazarnamdarrr@gmail.com>
2024-04-22 15:42:41 +0300

    Initial commit

---
+++ src/data.js
@@ -0,0 +1,30 @@
+// Data
+// (We are supposing that this data is coming from a database or an API as JSON data)
+
+export const account1 = {
+  owner: "Hazar Namdar",
+  movements: [200, 450, -400, 3000, -650, -130, 70, 1300],
+  interestRate: 1.2, // %
+  pin: 1111,
+};
+
+export const account2 = {
+  owner: "Taylan Yusuf Akgün",
+  movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
+  interestRate: 1.5,
+  pin: 3131,
+};
+
+export const account3 = {
+  owner: "Uğur Namdar",
+  movements: [200, -200, 340, -300, -20, 50, 400, -460],
+  interestRate: 0.7,
+  pin: 3333,
+};
+
+export const account4 = {
+  owner: "Barış Özgür Yaşar",
+  movements: [430, 1000, 700, 50, 90],
+  interestRate: 1,
+  pin: 4444,
+};
